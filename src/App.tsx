bfb8697fe306e457ab8055ac18f0232b0068862e
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-10-01 17:43:00 -0300

    fix gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-return */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { useStore } from './hooks/useStore'
@@ -7,12 +8,26 @@ import { AUTO_LANGUAGE } from './constants'
 import { LanguageSelector } from './components/LanguageSelector'
 import TextArea from './components/TextArea'
 import { SectionTypes } from './types.d'
+import { useEffect } from 'react'
+import { translate } from './components/traslate'
 
 function App () {
   const {
     toLanguage, fromLanguage, fromText, result, loading,
     interchangeLanguages, setFromLanguage, setToLanguage, setFromText, setResult
   } = useStore()
+
+  useEffect(() => {
+    if (fromText === '') return
+    translate({ fromLanguage, toLanguage, text: fromText })
+      .then(result => {
+      // se usa == por que compara null y undefined
+        if (result == null) return
+        setResult(result)
+      })
+      .catch(() => { setResult('error') })
+  }, [fromText])
+
   return (
     <Container fluid>
       <h1>translate</h1>
